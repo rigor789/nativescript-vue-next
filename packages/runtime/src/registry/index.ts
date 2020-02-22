68ca2e177424aa2bdf3dbc9e662f744dd6ffa065
rigor789 <rigor789@gmail.com>
2020-02-22 17:33:31 +0100

    style: wrap register calls in a block to allow prettier-ignore

--- packages/runtime/src/registry/index.ts
+++ packages/runtime/src/registry/index.ts
@@ -81,80 +81,97 @@ export function registerElement(
 // }
 
 // register built in elements
-// layouts
-registerElement(
-  'AbsoluteLayout',
-  () => require('@nativescript/core/ui/layouts/absolute-layout').AbsoluteLayout
-)
-registerElement(
-  'DockLayout',
-  () => require('@nativescript/core/ui/layouts/dock-layout').DockLayout
-)
-registerElement(
-  'FlexboxLayout',
-  () => require('@nativescript/core/ui/layouts/flexbox-layout').FlexboxLayout
-)
-registerElement(
-  'GridLayout',
-  () => require('@nativescript/core/ui/layouts/grid-layout').GridLayout
-)
-registerElement(
-  'StackLayout',
-  () => require('@nativescript/core/ui/layouts/stack-layout').StackLayout
-)
-registerElement(
-  'WrapLayout',
-  () => require('@nativescript/core/ui/layouts/wrap-layout').WrapLayout
-)
+// prettier-ignore
+{
+    // layouts
+    registerElement(
+        'AbsoluteLayout',
+        () => require('@nativescript/core/ui/layouts/absolute-layout').AbsoluteLayout
+    )
+    registerElement(
+        'DockLayout',
+        () => require('@nativescript/core/ui/layouts/dock-layout').DockLayout
+    )
+    registerElement(
+        'FlexboxLayout',
+        () => require('@nativescript/core/ui/layouts/flexbox-layout').FlexboxLayout
+    )
+    registerElement(
+        'GridLayout',
+        () => require('@nativescript/core/ui/layouts/grid-layout').GridLayout
+    )
+    registerElement(
+        'StackLayout',
+        () => require('@nativescript/core/ui/layouts/stack-layout').StackLayout
+    )
+    registerElement(
+        'WrapLayout',
+        () => require('@nativescript/core/ui/layouts/wrap-layout').WrapLayout
+    )
 
-// ContentViews
-registerElement(
-  'ContentView',
-  () => require('@nativescript/core/ui/content-view').ContentView
-)
-registerElement(
-  'ScrollView',
-  () => require('@nativescript/core/ui/scroll-view').ScrollView
-)
+    // ContentViews
+    registerElement(
+        'ContentView',
+        () => require('@nativescript/core/ui/content-view').ContentView
+    )
+    registerElement(
+        'ScrollView',
+        () => require('@nativescript/core/ui/scroll-view').ScrollView
+    )
 
-// ActionBar
-registerElement(
-  'ActionBar',
-  () => require('@nativescript/core/ui/action-bar').ActionBar
-)
-registerElement(
-  'ActionItem',
-  () => require('@nativescript/core/ui/action-bar').ActionItem
-)
-registerElement(
-  'NavigationButton',
-  () => require('@nativescript/core/ui/action-bar').NavigationButton
-)
+    // ActionBar
+    registerElement(
+        'ActionBar',
+        () => require('@nativescript/core/ui/action-bar').ActionBar
+    )
+    registerElement(
+        'ActionItem',
+        () => require('@nativescript/core/ui/action-bar').ActionItem
+    )
+    registerElement(
+        'NavigationButton',
+        () => require('@nativescript/core/ui/action-bar').NavigationButton
+    )
 
-// navigation
-registerElement('Frame', () => require('@nativescript/core/ui/frame').Frame)
-registerElement('Page', () => require('@nativescript/core/ui/page').Page)
+    // navigation
+    registerElement(
+        'Frame',
+        () => require('@nativescript/core/ui/frame').Frame
+    )
+    registerElement(
+        'Page',
+        () => require('@nativescript/core/ui/page').Page
+    )
 
-// html
-registerElement(
-  'HtmlView',
-  () => require('@nativescript/core/ui/html-view').HtmlView
-)
-registerElement(
-  'WebView',
-  () => require('@nativescript/core/ui/web-view').WebView
-)
+    // html
+    registerElement(
+        'HtmlView',
+        () => require('@nativescript/core/ui/html-view').HtmlView
+    )
+    registerElement(
+        'WebView',
+        () => require('@nativescript/core/ui/web-view').WebView
+    )
 
-// components
-registerElement(
-  'ActivityIndicator',
-  () => require('@nativescript/core/ui/activity-indicator').ActivityIndicator
-)
-registerElement('Button', () => require('@nativescript/core/ui/button').Button)
-registerElement(
-  'DatePicker',
-  () => require('@nativescript/core/ui/date-picker').DatePicker
-)
-registerElement('Image', () => require('@nativescript/core/ui/image').Image)
-registerElement('Label', () => require('@nativescript/core/ui/label').Label)
+    // components
+    registerElement(
+        'ActivityIndicator',
+        () => require('@nativescript/core/ui/activity-indicator').ActivityIndicator
+    )
+    registerElement(
+        'Button',
+        () => require('@nativescript/core/ui/button').Button
+    )
+    registerElement(
+        'DatePicker',
+        () => require('@nativescript/core/ui/date-picker').DatePicker
+    )
+    registerElement(
+        'Image',
+        () => require('@nativescript/core/ui/image').Image)
+    registerElement(
+        'Label',
+        () => require('@nativescript/core/ui/label').Label
+    )
+}
 // todo: more

5bfe727050b3ede4480ef0b0872734397dbc06eb
rigor789 <rigor789@gmail.com>
2020-03-01 13:42:08 +0100

    test: add more insertBefore tests

--- packages/runtime/__tests__/nodeOps.spec.ts
+++ packages/runtime/__tests__/nodeOps.spec.ts
@@ -1,6 +1,7 @@
 import { h, render, nodeOps } from '../src'
 import { ELEMENT_REF } from '@nativescript-vue/runtime'
 import { Frame } from '@nativescript/core/ui/frame'
+import { NSVNodeTypes } from '../src/nodes'
 
 describe('createElement', () => {
   test('NSVElement node has the right data', () => {
@@ -25,6 +26,10 @@ describe('createElement', () => {
     expect(root.childNodes.length).toBe(0)
     expect(root.text).toBe('hello world')
   })
+  test('NSVRoot has the right data', () => {
+    const root = nodeOps.createRoot()
+    expect(root.nodeType).toBe(NSVNodeTypes.ROOT)
+  })
   test('NSVElement is rendered correctly', () => {
     const root = nodeOps.createElement('frame')
     const wrapper = () => h('label')
@@ -32,7 +37,7 @@ describe('createElement', () => {
     // TODO: Complete
   })
   test('NSVElement - appendChild', () => {
-    const root = nodeOps.createElement('ContentView')
+    const root = nodeOps.createElement('StackLayout')
     const child = nodeOps.createElement('Label')
     expect(root.childNodes.length).toBe(0)
     root.appendChild(child)
@@ -42,13 +47,26 @@ describe('createElement', () => {
     expect(root.lastChild).toBe(child)
   })
   test('NSVElement - insertBefore', () => {
-    const root = nodeOps.createElement('ContentView')
+    const root = nodeOps.createElement('StackLayout')
     const child = nodeOps.createElement('Label')
+    const child2 = nodeOps.createElement('Label')
+
     expect(root.childNodes.length).toBe(0)
+
+    // insert without anchor
     root.insertBefore(child, null)
+
     expect(root.childNodes.length).toBe(1)
     expect(child.parentNode).toBe(root)
     expect(root.firstChild).toBe(child)
+    expect(root.lastChild).toBe(child)
+
+    // insert with anchor
+    root.insertBefore(child2, child)
+
+    expect(root.childNodes.length).toBe(2)
+    expect(child2.parentNode).toBe(root)
+    expect(root.firstChild).toBe(child2)
     expect(root.lastChild).toBe(child)
   })
   // TODO: Complete

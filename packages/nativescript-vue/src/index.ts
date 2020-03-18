d16657cd5c758a9336b572482d1e775dc371f3fd
rigor789 <rigor789@gmail.com>
2020-03-18 18:54:05 +0100

    feat: implement nativescript-vue package

--- packages/nativescript-vue/src/index.ts
+++ packages/nativescript-vue/src/index.ts
@@ -1,2 +1,48 @@
 //
+import {
+  compile,
+  CompilerOptions,
+  CompilerError
+} from '@nativescript-vue/compiler'
+import {
+  registerRuntimeCompiler,
+  RenderFunction,
+  warn
+} from '@nativescript-vue/runtime'
+import * as runtime from '@nativescript-vue/runtime'
+import { isString, NOOP } from '@vue/shared'
+
+const compileCache: Record<string, RenderFunction> = Object.create(null)
+
+function compileToFunction(
+  template: string,
+  options?: CompilerOptions
+): RenderFunction {
+  if (!isString(template)) {
+    __DEV__ && warn('invalid template option: ', template)
+    return NOOP
+  }
+
+  const key = template
+  const cached = compileCache[key]
+
+  if (cached) {
+    return cached
+  }
+
+  const { code } = compile(template, {
+    hoistStatic: true,
+    onError(err: CompilerError) {
+      // todo
+    },
+    ...options
+  })
+
+  const render = new Function('Vue', code)(runtime) as RenderFunction
+  return (compileCache[key] = render)
+}
+
+registerRuntimeCompiler(compileToFunction)
+
+export { compileToFunction as compile }
 export * from '@nativescript-vue/runtime'
---
+++ packages/nativescript-vue/src/runtime.ts
@@ -0,0 +1,7 @@
+// This only exports the runtime
+// still undecided if we need a runtime-only build for NativeScript
+// since the size difference is not an issue
+// plus - we might always want to have the compiler included
+// for compiling special components on the go?
+
+export * from '@nativescript-vue/runtime'

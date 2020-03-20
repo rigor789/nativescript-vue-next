1167ac8f6a8a15e67104f94a6b00694ee09e8039
Igor Randjelovic <rigor789@gmail.com>
2020-03-20 18:22:48 +0100

    chore: remove commented code

--- packages/global.d.ts
+++ packages/global.d.ts
@@ -2,12 +2,6 @@
 declare var __DEV__: boolean
 declare var __TEST__: boolean
 
-// todo: remove after PR merged into core
-// https://github.com/NativeScript/NativeScript/pull/8423
-// declare module '@nativescript/core/css/css-tree-parser' {
-//   export function cssTreeParse(css: any, source: any): any
-// }
-
 declare module 'set-value' {
   function set(target: any, path: any, value: any, options?: any): void
 

7261b165ef00d654df2e916365acb8b687a7b03b
rigor789 <rigor789@gmail.com>
2020-04-04 17:17:16 +0200

    test(compiler): enable transform tests with alpha.11

--- packages/compiler/__tests__/transforms/vOn.spec.ts
+++ packages/compiler/__tests__/transforms/vOn.spec.ts
@@ -1,40 +1,38 @@
-// todo: uncomment when vue-next#907 is released
-test.todo('uncomment when vue-next#907 is released')
-// import {
-//   baseParse as parse,
-//   transform,
-//   CompilerOptions,
-//   ElementNode,
-//   ObjectExpression,
-//   VNodeCall,
-//   transformElement,
-//   transformExpression
-// } from '@vue/compiler-core'
-// import { transformOn } from '../../src/transforms/vOn'
-//
-// function parseWithVOn(template: string, options: CompilerOptions = {}) {
-//   const ast = parse(template)
-//   transform(ast, {
-//     nodeTransforms: [transformExpression, transformElement],
-//     directiveTransforms: {
-//       on: transformOn
-//     },
-//     ...options
-//   })
-//   return {
-//     root: ast,
-//     props: (((ast.children[0] as ElementNode).codegenNode as VNodeCall)
-//       .props as ObjectExpression).properties
-//   }
-// }
-//
-// describe('compiler: transform v-on', () => {
-//   it('should work w/ prefixIdentifiers: true', () => {
-//     const {
-//       props: [prop]
-//     } = parseWithVOn(`<Label @tap.once="test"/>`, {
-//       prefixIdentifiers: true
-//     })
-//     expect(prop).toMatchSnapshot()
-//   })
-// })
+import {
+  baseParse as parse,
+  transform,
+  CompilerOptions,
+  ElementNode,
+  ObjectExpression,
+  VNodeCall,
+  transformElement,
+  transformExpression
+} from '@vue/compiler-core'
+import { transformOn } from '../../src/transforms/vOn'
+
+function parseWithVOn(template: string, options: CompilerOptions = {}) {
+  const ast = parse(template)
+  transform(ast, {
+    nodeTransforms: [transformExpression, transformElement],
+    directiveTransforms: {
+      on: transformOn
+    },
+    ...options
+  })
+  return {
+    root: ast,
+    props: (((ast.children[0] as ElementNode).codegenNode as VNodeCall)
+      .props as ObjectExpression).properties
+  }
+}
+
+describe('compiler: transform v-on', () => {
+  it('should work w/ prefixIdentifiers: true', () => {
+    const {
+      props: [prop]
+    } = parseWithVOn(`<Label @tap.once="test"/>`, {
+      prefixIdentifiers: true
+    })
+    expect(prop).toMatchSnapshot()
+  })
+})
--- packages/compiler/__tests__/transforms/vShow.spec.ts
+++ packages/compiler/__tests__/transforms/vShow.spec.ts
@@ -1,43 +1,41 @@
-// todo: uncomment when vue-next#907 is released
-test.todo('uncomment when vue-next#907 is released')
-// import {
-//   baseParse as parse,
-//   transform,
-//   generate,
-//   CompilerOptions,
-//   transformElement
-// } from '@vue/compiler-core'
-// import { transformShow } from '../../src/transforms/vShow'
-// import { DOMErrorCodes } from '../../src/errors'
-//
-// function transformWithShow(template: string, options: CompilerOptions = {}) {
-//   const ast = parse(template)
-//   transform(ast, {
-//     nodeTransforms: [transformElement],
-//     directiveTransforms: {
-//       show: transformShow
-//     },
-//     ...options
-//   })
-//   return ast
-// }
-//
-// describe('compiler: v-show transform', () => {
-//   test('simple expression', () => {
-//     const ast = transformWithShow(`<Label v-show="a"/>`)
-//
-//     expect(generate(ast).code).toMatchSnapshot()
-//   })
-//
-//   test('should raise error if has no expression', () => {
-//     const onError = jest.fn()
-//     transformWithShow(`<Label v-show/>`, { onError })
-//
-//     expect(onError).toHaveBeenCalledTimes(1)
-//     expect(onError).toHaveBeenCalledWith(
-//       expect.objectContaining({
-//         code: DOMErrorCodes.X_V_SHOW_NO_EXPRESSION
-//       })
-//     )
-//   })
-// })
+import {
+  baseParse as parse,
+  transform,
+  generate,
+  CompilerOptions,
+  transformElement
+} from '@vue/compiler-core'
+import { transformShow } from '../../src/transforms/vShow'
+import { DOMErrorCodes } from '../../src/errors'
+
+function transformWithShow(template: string, options: CompilerOptions = {}) {
+  const ast = parse(template)
+  transform(ast, {
+    nodeTransforms: [transformElement],
+    directiveTransforms: {
+      show: transformShow
+    },
+    ...options
+  })
+  return ast
+}
+
+describe('compiler: v-show transform', () => {
+  test('simple expression', () => {
+    const ast = transformWithShow(`<Label v-show="a"/>`)
+
+    expect(generate(ast).code).toMatchSnapshot()
+  })
+
+  test('should raise error if has no expression', () => {
+    const onError = jest.fn()
+    transformWithShow(`<Label v-show/>`, { onError })
+
+    expect(onError).toHaveBeenCalledTimes(1)
+    expect(onError).toHaveBeenCalledWith(
+      expect.objectContaining({
+        code: DOMErrorCodes.X_V_SHOW_NO_EXPRESSION
+      })
+    )
+  })
+})

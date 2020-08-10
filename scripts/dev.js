16fdbf6203f2bed42ea9e8403bdd068c5842ba8e
Igor Randjelovic <rigor789@gmail.com>
2020-08-10 13:11:44 +0200

    chore: default builds to esm-bundler format

--- scripts/dev.js
+++ scripts/dev.js
@@ -22,6 +22,7 @@ const args = require('minimist')(process.argv.slice(2))
 const targets = args._
 const formats = args.formats || args.f
 const sourceMap = args.sourcemap || args.s
+const buildAllMatching = args.all || args.a
 const commit = execa.sync('git', ['rev-parse', 'HEAD']).stdout.slice(0, 7)
 
 run()
@@ -46,7 +47,7 @@ async function buildAll(targets) {
           [
             `COMMIT:${commit}`,
             `TARGET:${target}`,
-            `FORMATS:${formats || 'global'}`,
+            `FORMATS:${formats || 'esm-bundler'}`,
             sourceMap ? `SOURCE_MAP:true` : ``,
           ]
             .filter(Boolean)

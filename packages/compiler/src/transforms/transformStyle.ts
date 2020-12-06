c0e6e175a4db654ba55dbd61e47264cd657f461a
Adnan Ali <adnan.ali@gmail.com>
2020-12-06 12:59:49 -0500

    chore: Update @vue/* dependencies and tests.

--- packages/compiler/src/transforms/transformStyle.ts
+++ packages/compiler/src/transforms/transformStyle.ts
@@ -3,7 +3,8 @@ import {
   NodeTypes,
   createSimpleExpression,
   SimpleExpressionNode,
-  SourceLocation
+  SourceLocation,
+  ConstantTypes,
 } from '@vue/compiler-core'
 
 // Parse inline CSS strings for static style attributes into an object.
@@ -23,7 +24,7 @@ export const transformStyle: NodeTransform = (node, context) => {
           arg: createSimpleExpression(`style`, true, p.loc),
           exp: parseInlineCSS(p.value.content, p.loc),
           modifiers: [],
-          loc: p.loc
+          loc: p.loc,
         }
       }
     })
@@ -38,11 +39,16 @@ function parseInlineCSS(
   loc: SourceLocation
 ): SimpleExpressionNode {
   const res: Record<string, string> = {}
-  cssText.split(listDelimiterRE).forEach(item => {
+  cssText.split(listDelimiterRE).forEach((item) => {
     if (item) {
       const tmp = item.split(propertyDelimiterRE)
       tmp.length > 1 && (res[tmp[0].trim()] = tmp[1].trim())
     }
   })
-  return createSimpleExpression(JSON.stringify(res), false, loc, true)
+  return createSimpleExpression(
+    JSON.stringify(res),
+    false,
+    loc,
+    ConstantTypes.CAN_STRINGIFY
+  )
 }
